8400699eb2b6387cbfeb67957b02506778390b0a
Egga <github@egga.de>
2018-09-30 09:53:48 +0200

    make it an interface

--- src/main/java/de/egga/RiskyService.java
+++ src/main/java/de/egga/RiskyService.java
@@ -4,24 +4,13 @@ import io.vavr.control.Either;
 
 import java.util.Optional;
 
-import static io.vavr.control.Either.right;
-import static java.util.Optional.empty;
+public interface RiskyService {
 
-public class RiskyService {
+    public Integer getNull(String id);
 
-    public Integer getNull(String id) {
-        return null;
-    }
+    public Integer throwException(String id);
 
-    public Integer throwException(String id) {
-        throw new RuntimeException();
-    }
+    public Optional<Integer> getOptional(String id);
 
-    public Optional<Integer> getOptional(String id) {
-        return empty();
-    }
-
-    public Either<Integer, String> getEither(String id) {
-        return right("404");
-    }
+    public Either<Integer, String> getEither(String id);
 }
